2ed4e4384a87300d70c887c3492e0b2ebbf407c8
Pedro <pedrocarsamar@gmail.com>
2024-09-18 00:05:22 -0300

    DAG update.

--- dag.R
+++ dag.R
@@ -45,17 +45,18 @@ zgg1_label <- c(W = "Gasto Social",
                 U = "Confusor Não-Observável",
                 Wt1 = "Gasto Social Anterior",
                 R = "Arrecadação",
-                H = "Humor Político")
+                H = "Humor Político",
+                I = "Ideologia Partidária")
 
 zgg1_coord <- list(
   x = c(P = 1, X = 2, R = 3, W = 4,
-        O = 3, Wt1 = 4,
+        O = 3, Wt1 = 3.5, I = 3.5,
         H = 3, U = 3),
   y = c(P = 0, X = 0, R = 0.25, W = 0,
-        O = 1, Wt1 = 1,
+        O = 1, Wt1 = 1, I = -1,
         H = -0.25, U = -1))
 
-zgg1 <- dagify(W ~ R + H + O + Wt1 + U,
+zgg1 <- dagify(W ~ R + H + O + Wt1 + I + U,
                R ~ X, H ~ X + R,
                X ~ P + U,
               labels = zgg1_label,
@@ -68,7 +69,7 @@ zgg1 <- zgg1 %>%
   geom_dag_edges() +
   geom_dag_label_repel(aes(label = label)) +
   geom_dag_text(parse = TRUE, label = c(
-    expression(H[t-1]),"O",expression(P[t-1]), expression(R[t-1]), "U","W", 
+    expression(H[t-1]), "I", "O", expression(P[t-1]), expression(R[t-1]), "U","W", 
     expression(W[t-1]), expression(X[t-1])
     ))
 
